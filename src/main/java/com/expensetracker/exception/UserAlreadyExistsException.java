6e4728656f242cecdcc338348239fbd8e429d0fe
Aditya <adityadharawat@gmail.com>
2025-05-15 19:28:45 +0530

    src updated

--- src/main/java/com/expensetracker/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.expensetracker.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/expensetracker/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.expensetracker.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
